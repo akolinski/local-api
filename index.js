5b5723bc23f24738855d9b5ab31858312ec2b83c
Adam Kolinski <adam.p.kolinski@gmail.com>
2020-07-06 16:09:21 +1000

    Building a local API with express for development purposes

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('A yellow icon approaches!');
+});
+
+app.listen(3000, () => console.log('LOCAL API app listening on port 3000!'));
