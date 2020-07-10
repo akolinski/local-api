195b0918d8deb23247052c43ee309e3ad79d9086
Adam Kolinski <adam.p.kolinski@gmail.com>
2020-07-10 15:06:00 +1000

    Updated API

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ app.get('/', (req, res) => {
     res.send('A yellow icon approaches!');
 });
 
-app.listen(3000, () => console.log('LOCAL API app listening on port 3000 @ http://localhost:3000'));
+app.listen(3000, () => console.log('The API app is now listening on port 3000 @ http://localhost:3000'));
