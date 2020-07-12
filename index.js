de1f35d598f19b41fa09f48d69c80970df2368be
Adam Kolinski <adam.p.kolinski@gmail.com>
2020-07-13 09:02:50 +1000

    fixing index file

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ app.get('/', (req, res) => {
     res.send('A yellow icon approaches!');
 });
 
+app.get('/api/domain_structure?domain=pat-reading', (request, response) => {
+    response.send('Hello, /');
+});
+
+
 app.listen(3000, () => console.log('The API app is now listening on port 3000 @ http://localhost:3000'));
